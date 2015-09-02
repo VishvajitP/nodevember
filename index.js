d5bdcc733ffe02acf21af7affc27605b2c1ba2fa
Vishvajit Pathak <vishvajit.pathak@incentius.com>
2015-09-02 11:55:24 +0530

    basic hello server setup

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 var Hapi = require('hapi');
 
+var server = new Hapi.Server();
 
+server.connection({ host:'localhost', port: 8080 });
+
+server.route({
+
+    method: 'GET',
+    path: '/',
+    handler:function(request, reply){
+        reply('server is running')
+    }
+});
+
+server.start( function(){
+        server.log('info', 'Server running at: ' + server.info.uri);
+});
